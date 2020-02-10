1e3f4fb322a6625bf93fe6b9b623fa4c97244d24
Vijay Pratap Singh <vijay.pratap@mantralabsglobal.com>
2020-02-10 14:56:05 +0000

    Update serializers.py

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -4,4 +4,4 @@ from apps.users.models import BaseUser, Relationship
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = BaseUser
-        fields = ['id', 'first_name', 'last_name', 'middle_name', 'mobile', 'otp', 'facebook_id', 'google_id']
+        fields = ['id', 'first_name', 'last_name', 'middle_name', 'mobile', 'otp', 'facebook_id', 'google_id', 'email']
