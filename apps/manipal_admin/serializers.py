dfe4751492528353fc058c5b47be65f791b7e8b3
madhu <madhusudhan@mantralabsglobal.com>
2020-02-27 18:23:19 +0530

    Manipal admin serialiser fix

--- apps/manipal_admin/serializers.py
+++ apps/manipal_admin/serializers.py
@@ -5,4 +5,4 @@ from apps.manipal_admin.models import ManipalAdmin
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = ManipalAdmin
-        fields = ['id', 'first_name', 'last_name', 'middle_name', 'email']
+        fields = ['id', 'name', 'email']
