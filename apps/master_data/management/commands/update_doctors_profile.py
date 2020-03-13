164b31fa3548175a17b7e35ee083876314747dbf
madhu <madhusudhan@mantralabsglobal.com>
2020-03-13 14:48:47 +0530

    PATIENT_PROFILE_SYNC_API

--- apps/master_data/management/commands/update_doctors_profile.py
+++ apps/master_data/management/commands/update_doctors_profile.py
@@ -4,21 +4,21 @@ import requests
 from django.core.management import BaseCommand
 from apps.doctors.models import Doctor
 
-
+from manipal_api.settings import PATIENT_PROFILE_SYNC_API
 class Command(BaseCommand):
     help = "Update Doctors Profile Information"
 
     def handle(self, *args, **options):
         try:
             response_data = requests.request(
-                'GET', 'https://www.manipalhospitals.com/PatientAPI').text
+                'GET', PATIENT_PROFILE_SYNC_API).text
             response_data = json.loads(response_data)
             for each_doctor_record in response_data:
                 if not each_doctor_record['DoctorCode']:
                     continue
 
                 doctor_details = dict()
-                for index, key in enumerate(sorted(each_doctor_record.keys())):
+                for key in sorted(each_doctor_record.keys()):
                     if key in ['qualification', 'designation', 'field_expertise',
                                'languages_spoken',
                                'awards_achievements', 'fellowship_membership', 'photo']:
