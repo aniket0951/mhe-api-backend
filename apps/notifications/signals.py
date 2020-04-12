350080474d43ed2b1bc29d567df3eb50a6b0bc1a
vijay.pratap <vijay.pratap@mantralabsglobal.com>
2020-04-12 15:43:03 +0530

    Refactored code

--- apps/notifications/signals.py
+++ apps/notifications/signals.py
@@ -24,74 +24,84 @@ def send_new_appointment_notification(sender, **kwargs):
         if appointment_instance.family_member:
             user_message = "Dear {0}, Your Appointment has been booked by {6} with {1} on {2} at {3} with appointment id:{4} at {5}".format(appointment_instance.family_member.first_name, appointment_instance.doctor.name,
                                                                                                                                             appointment_instance.appointment_date, appointment_instance.appointment_slot, appointment_instance.appointment_identifier, appointment_instance.hospital.address, appointment_instance.patient.first_name)
+            member = FamilyMember.objects.filter(
+                id=appointment_instance.family_member, patient_info_id=appointment_instance.patient)
+            if Patient.objects.filter(uhid_number__isnull=False, uhid_number=member.uhid_number).exists():
+                patient_member = Patient.objects.filter(
+                    uhid_number=member.uhid_number).first()
+                notification_data["recipient"] = patient_member.id
+                notification_data["title"] = "Doctor Appointment Booked Successfully"
+                notification_data["message"] = user_message
+                send_push_notification.delay(notification_data = notification_data)
+                schedule_time = datetime.combine(appointment_instance.appointment_date, 
+                                                 appointment_instance.appointment_slot) - timedelta(hours=7, minutes=30)
+                notification_data["title"] = "Reminder: Doctor Appointment"
+                send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)    
         notification_data["recipient"] = appointment_instance.patient.id
-        notification_data["title"] = "New Appointment is created"
+        notification_data["title"] = "Doctor Appointment Booked Successfully"
         notification_data["message"] = user_message
-        mobile_notification_serializer = MobileNotificationSerializer(
-            data=notification_data)
-        mobile_notification_serializer.is_valid(raise_exception=True)
-        notification_instance = mobile_notification_serializer.save()
-        send_push_notification.delay(registration_id=notification_instance.recipient.device.token,
-                                    data_message={"title": notification_instance.title,
-                                                "message": notification_instance.message}, low_priority=False)
+        send_push_notification.delay(notification_data = notification_data)
         schedule_time = datetime.combine(
             appointment_instance.appointment_date, appointment_instance.appointment_slot) - timedelta(hours=7, minutes=30)
-        send_push_notification.apply_async(kwargs={"registration_id": notification_instance.recipient.device.token,
-                                                "data_message": {"title": "Appointment Reminder",
-                                                                    "message": "Reminder:" + notification_instance.message}, "low_priority": False}, eta=schedule_time)
+        notification_data["title"] = "Reminder: Doctor Appointment"
+        send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)
+        
 
 
 @receiver(post_save, sender=Report)
 def send_new_report_notification(sender, **kwargs):
     new_report = kwargs['instance']
+    notification_data = {}
     if kwargs["created"]:
         patient = Patient.objects.filter(uhid_number=new_report.uhid).first()
         if patient:
-            notification_data = {}
             notification_data["recipient"] = patient.id
-            notification_data["title"] = "New Report is Created"
+            notification_data["title"] = "Report is Created"
             notification_data["message"] = "Hey {0}, Your Report is Created".format(
                 patient.first_name)
-            mobile_notification_serializer = MobileNotificationSerializer(
-                data=notification_data)
-            mobile_notification_serializer.is_valid(raise_exception=True)
-            notification_instance = mobile_notification_serializer.save()
-            send_push_notification.delay(registration_id=notification_instance.recipient.device.token, data_message={
-                                        "title": notification_instance.title, "message": notification_instance.message}, low_priority=False)
+            send_push_notification.delay(notification_data = notification_data)
 
 
 @receiver(post_save, sender=HealthPackageAppointment)
 def send_new_health_package_appointment_notification(sender, **kwargs):
     appointment_instance = kwargs['instance']
+    notification_data = {}
     if kwargs["created"]:
-        notification_data = {}
         user_message = ""
+        if appointment_instance.payment_done_for_family_member:
+            patient = appointment_instance.payment.patient
+            user_message = "Hi {0}, your health package appointment is booked on {1} at {2}".format(
+                patient.first_name, appointment_instance.appointment_date, appointment_instance.appointment_slot)
+            notification_data["recipient"] = patient
+            notification_data["title"] = "Health PackageAppointment Booked Successfully"
+            notification_data["message"] = user_message
+            send_push_notification.delay(notification_data = notification_data)
+            send_push_notification.delay(notification_data = notification_data)
+            schedule_time = datetime.combine(
+                appointment_instance.appointment_date, appointment_instance.appointment_slot) - timedelta(hours=7, minutes=30)
+            notification_data["title"] = "Reminder: Health Package Appointment"
+            send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)
+            
+
         patient = Patient.objects.filter(
             uhid_number=appointment_instance.payment.uhid_number).first()
         if patient:
             user_message = "Hi {0}, your health package appointment is booked on {1} at {2}".format(
                 patient.first_name, appointment_instance.appointment_date, appointment_instance.appointment_slot)
             notification_data["recipient"] = patient.id
-            notification_data["title"] = "New Health PackageAppointment is created"
+            notification_data["title"] = "Health PackageAppointment Booked Successfully"
             notification_data["message"] = user_message
-            mobile_notification_serializer = MobileNotificationSerializer(
-                data=notification_data)
-            mobile_notification_serializer.is_valid(raise_exception=True)
-            notification_instance = mobile_notification_serializer.save()
-            send_push_notification.delay(registration_id=notification_instance.recipient.device.token,
-                                        data_message={
-                                            "title": notification_instance.title, "message": notification_instance.message},
-                                        low_priority=False)
+            send_push_notification.delay(notification_data = notification_data)
             schedule_time = datetime.combine(
                 appointment_instance.appointment_date, appointment_instance.appointment_slot) - timedelta(hours=7, minutes=30)
-            send_push_notification.apply_async(kwargs={"registration_id": notification_instance.recipient.device.token,
-                                                    "data_message": {"title": "Appointment Reminder",
-                                                                        "message": "Reminder:" + notification_instance.message}, "low_priority": False}, eta=schedule_time)
+            notification_data["title"] = "Reminder: Health Package Appointment"
+            send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)
 
 
 @receiver(post_save, sender=HomeCollectionAppointment)
 def send_new_home_collection_appointment_notification(sender, **kwargs):
     appointment_instance = kwargs['instance']
+    notification_data = {}
     if kwargs["created"]:
         patient = Patient.objects.filter(id=appointment_instance.patient).first()
         if appointment_instance.family_member:
@@ -101,36 +111,29 @@ def send_new_home_collection_appointment_notification(sender, **kwargs):
                 patient_member = Patient.objects.filter(
                     uhid_number=member.uhid_number).first()
                 notification_data["recipient"] = patient_member.id
-                notification_data["title"] = "New Home Collection Appointment is created"
+                notification_data["title"] = "Home Collection Appointment Booked Successfully"
                 notification_data["message"] = "You have a home collection appointment on {0}".format(
                     appointment_instance.appointment_date)
-                mobile_notification_serializer = MobileNotificationSerializer(
-                    data=notification_data)
-                mobile_notification_serializer.is_valid(raise_exception=True)
-                notification_instance = mobile_notification_serializer.save()
+                send_push_notification.delay(notification_data = notification_data)
                 schedule_time = appointment_instance.appointment_date - \
                     timedelta(hours=7, minutes=30)
-                send_push_notification.apply_async(kwargs={"registration_id": notification_instance.recipient.device.token,
-                                                        "data_message": {"title": "Appointment Reminder",
-                                                                            "message": "Reminder:" + notification_instance.message}, "low_priority": False}, eta=schedule_time)
+                notification_data["title"] = "Reminder: Home Collection Appointment"
+                send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)
             notification_data["recipient"] = patient.id
-            notification_data["title"] = "New Home Collection Appointment is created"
+            notification_data["title"] = "Home Collection Appointment Booked Successfully"
             notification_data["message"] = "You have a home collection appointment on {0}".format(
                 appointment_instance.appointment_date)
-            mobile_notification_serializer = MobileNotificationSerializer(
-                data=notification_data)
-            mobile_notification_serializer.is_valid(raise_exception=True)
-            notification_instance = mobile_notification_serializer.save()
+            send_push_notification.delay(notification_data = notification_data)
             schedule_time = appointment_instance.appointment_date - \
                 timedelta(hours=7, minutes=30)
-            send_push_notification.apply_async(kwargs={"registration_id": notification_instance.recipient.device.token,
-                                                    "data_message": {"title": "Appointment Reminder",
-                                                                        "message": "Reminder:" + notification_instance.message}, "low_priority": False}, eta=schedule_time)
+            notification_data["title"] = "Reminder: Home collection Appointment"
+            send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)
 
 
 @receiver(post_save, sender=PatientServiceAppointment)
 def send_new_patient_service_appointment_notification(sender, **kwargs):
     appointment_instance = kwargs['instance']
+    notification_data = {}
     if kwargs["created"]:
         patient = Patient.objects.filter(id=appointment_instance.patient).first()
         if appointment_instance.family_member:
@@ -140,28 +143,20 @@ def send_new_patient_service_appointment_notification(sender, **kwargs):
                 patient_member = Patient.objects.filter(
                     uhid_number=member.uhid_number).first()
                 notification_data["recipient"] = patient_member.id
-                notification_data["title"] = "Home Collection Appointment is created"
-                notification_data["message"] = "Reminder: Hi {0},You have a home collection appointent on {1}".format(
+                notification_data["title"] = "Service Appointment Booked Successfully"
+                notification_data["message"] = "Hi {0},You have a Service appointent on {1}".format(
                     patient.first_name, appointment_instance.appointment_date)
-                mobile_notification_serializer = MobileNotificationSerializer(
-                    data=notification_data)
-                mobile_notification_serializer.is_valid(raise_exception=True)
-                notification_instance = mobile_notification_serializer.save()
+                send_push_notification.delay(notification_data = notification_data)
                 schedule_time = appointment_instance.appointment_date - \
                     timedelta(hours=7, minutes=30)
-                send_push_notification.apply_async(kwargs={"registration_id": notification_instance.recipient.device.token,
-                                                        "data_message": {"title": "Home Collection Appointment Reminder",
-                                                                            "message": "Reminder:" + notification_instance.message}, "low_priority": False}, eta=schedule_time)
+                notification_data["Title"] = "Reminder: Service Appointment"
+                send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)
             notification_data["recipient"] = patient.id
-            notification_data["title"] = "Home Collection Appointment is created"
-            notification_data["message"] = "Reminder: Hi {0},You have a home collection appointent on {1}".format(
+            notification_data["title"] = "Service Appointment Booked Successfully"
+            notification_data["message"] = "Hi {0},You have a Service appointment on {1}".format(
                 patient.first_name, appointment_instance.appointment_date)
-            mobile_notification_serializer = MobileNotificationSerializer(
-                data=notification_data)
-            mobile_notification_serializer.is_valid(raise_exception=True)
-            notification_instance = mobile_notification_serializer.save()
+            send_push_notification.delay(notification_data = notification_data)
             schedule_time = appointment_instance.appointment_date - \
                 timedelta(hours=7, minutes=30)
-            send_push_notification.apply_async(kwargs={"registration_id": notification_instance.recipient.device.token,
-                                                    "data_message": {"title": "Home Collection Appointment Reminder",
-                                                                        "message": "Reminder:" + notification_instance.message}, "low_priority": False}, eta=schedule_time)
+            notification_data["title"] = "Reminder: Service Appointment"
+            send_push_notification.apply_async(kwargs={"notification_data": notification_data}, eta=schedule_time)
--- apps/notifications/tasks.py
+++ apps/notifications/tasks.py
@@ -2,11 +2,18 @@ from manipal_api.celery import app
 from pyfcm import FCMNotification
 from fcm_django.models import FCMDevice
 from manipal_api.settings import FCM_API_KEY
+from .serializers import MobileNotificationSerializer
 
 @app.task(bind=True, name="push_notifications")
 def send_push_notification(self,**kwargs):
+    notification_data = kwargs["notification_data"]
     fcm = FCMNotification(api_key=FCM_API_KEY)
-    result = fcm.notify_single_device(**kwargs)
+    mobile_notification_serializer = MobileNotificationSerializer(
+                data=notification_data)
+    mobile_notification_serializer.is_valid(raise_exception=True)
+    notification_instance = mobile_notification_serializer.save()
+    result = fcm.notify_single_device(registration_id=notification_instance.recipient.device.token, data_message={
+                                        "title": notification_instance.title, "message": notification_instance.message}, low_priority=False)
 
 
 
