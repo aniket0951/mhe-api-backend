71a725725e84a84d202e60a2e86765d3c8300dec
Cloud User <ec2-user@ip-173-17-203-8.ap-south-1.compute.internal>
2020-05-03 08:52:10 +0000

    CELERY IAM role changes

--- manipal_api/celery.py
+++ manipal_api/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manipal_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manipal_api.settings.server')
 
 app = Celery('manipal_api')
 
