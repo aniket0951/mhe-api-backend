0e3f0f2a3973655f085ddf815d34c1612416104b
madhu <madhusudhan@mantralabsglobal.com>
2020-05-05 13:49:22 +0530

    DB router chnages

--- manipal_api/settings/server.py
+++ manipal_api/settings/server.py
@@ -162,8 +162,6 @@ WSGI_APPLICATION = 'manipal_api.wsgi.application'
 # Database (Postgresql) Settings
 DATABASES = {
     'default': {
-    },
-    'write_db': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': env('MANIPAL_DB_NAME'),
         'USER': env('MANIPAL_DB_USER'),
