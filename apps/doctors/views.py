5edda5ef894bfb84be16779e9239471fd54ec36d
vijay.pratap <vijay.pratap@mantralabsglobal.com>
2020-06-24 13:36:59 +0530

    Added condition check for no records

--- apps/doctors/views.py
+++ apps/doctors/views.py
@@ -108,41 +108,43 @@ class DoctorSlotAvailability(ProxyView):
         root = ET.fromstring(response.content)
         slots = root.find("timeSlots").text
         price = root.find("price").text
+        status = root.find("Status").text
         hv_price, vc_price, pr_price = 0, 0, 0
-        hv_price, *vc_pr_price = price.split("-")
-        if vc_pr_price:
-            vc_price, *pr_price = vc_pr_price
-            if pr_price:
-                pr_price, *rem = pr_price
         morning_slot, afternoon_slot, evening_slot, slot_list = [], [], [], []
-        if slots:
-            slot_list = ast.literal_eval(slots)
-        response = {}
-        for slot in slot_list:
-            time_format = ""
-            appointment_type = "HV"
-            if "HVVC" in slot['startTime']:
-                time_format = '%d %b, %Y %I:%M:%S %p(HVVC)'
-                appointment_type = "HVVC"
-            elif "VC" in slot['startTime']:
-                time_format = '%d %b, %Y %I:%M:%S %p(VC)'
-                appointment_type = "VC"
-            elif "PR" in slot['startTime']:
-                time_format = '%d %b, %Y %I:%M:%S %p(PR)'
-                appointment_type = "PR"
-            else:
-                time_format = '%d %b, %Y %I:%M:%S %p(HV)'
-            time = datetime.strptime(
-                slot['startTime'], time_format).time()
-            if time.hour < 12:
-                morning_slot.append({"slot": time.strftime(
-                    "%I:%M:%S %p"), "type": appointment_type})
-            elif (time.hour >= 12) and (time.hour < 17):
-                afternoon_slot.append({"slot": time.strftime(
-                    "%I:%M:%S %p"), "type": appointment_type})
-            else:
-                evening_slot.append({"slot": time.strftime(
-                    "%I:%M:%S %p"), "type": appointment_type})
+        if status == "SUCCESS":
+            hv_price, *vc_pr_price = price.split("-")
+            if vc_pr_price:
+                vc_price, *pr_price = vc_pr_price
+                if pr_price:
+                    pr_price, *rem = pr_price
+            if slots:
+                slot_list = ast.literal_eval(slots)
+            response = {}
+            for slot in slot_list:
+                time_format = ""
+                appointment_type = "HV"
+                if "HVVC" in slot['startTime']:
+                    time_format = '%d %b, %Y %I:%M:%S %p(HVVC)'
+                    appointment_type = "HVVC"
+                elif "VC" in slot['startTime']:
+                    time_format = '%d %b, %Y %I:%M:%S %p(VC)'
+                    appointment_type = "VC"
+                elif "PR" in slot['startTime']:
+                    time_format = '%d %b, %Y %I:%M:%S %p(PR)'
+                    appointment_type = "PR"
+                else:
+                    time_format = '%d %b, %Y %I:%M:%S %p(HV)'
+                time = datetime.strptime(
+                    slot['startTime'], time_format).time()
+                if time.hour < 12:
+                    morning_slot.append({"slot": time.strftime(
+                        "%I:%M:%S %p"), "type": appointment_type})
+                elif (time.hour >= 12) and (time.hour < 17):
+                    afternoon_slot.append({"slot": time.strftime(
+                        "%I:%M:%S %p"), "type": appointment_type})
+                else:
+                    evening_slot.append({"slot": time.strftime(
+                        "%I:%M:%S %p"), "type": appointment_type})
         response["morning_slot"] = morning_slot
         response["afternoon_slot"] = afternoon_slot
         response["evening_slot"] = evening_slot
