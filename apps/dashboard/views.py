96250dc508b775dd866b99bd02e44b526681931d
vijay.pratap <vijay.pratap@mantralabsglobal.com>
2020-11-05 11:22:04 +0530

    Added hospital wise total,opd, vc count

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db.models import Sum
 
 from apps.appointments.models import Appointment, HealthPackageAppointment
 from apps.appointments.serializers import AppointmentSerializer
