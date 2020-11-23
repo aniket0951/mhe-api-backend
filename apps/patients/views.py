b72065eb3b4afde57326c0e31a98fdbe49de757d
vijay.pratap <vijay.pratap@mantralabsglobal.com>
2020-11-23 16:04:37 +0530

    changed sex to gender

--- apps/patients/views.py
+++ apps/patients/views.py
@@ -1169,7 +1169,7 @@ class FamilyMemberViewSet(custom_viewsets.ModelViewSet):
         uhid_user_info['mobile'] = request.data.get("mobile")
         uhid_user_info['age'] = request.data.get("age")
         uhid_user_info['gender'] = request.data.get("gender")
-        uhid_user_info['uhid_numher'] = uhid_number
+        uhid_user_info['uhid_number'] = uhid_number
         uhid_user_info['mobile_verified']=True
         uhid_user_info['is_visible']=True
         uhid_user_info['email_verified']=True
