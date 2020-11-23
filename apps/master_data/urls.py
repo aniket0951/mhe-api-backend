e16d7cecef0126eb14d6e1c5d7628bd8030c6a15
vijay.pratap <vijay.pratap@mantralabsglobal.com>
2020-11-23 14:09:38 +0530

    resolved merge conflict

--- apps/master_data/urls.py
+++ apps/master_data/urls.py
@@ -4,9 +4,10 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import (AmbulanceContactViewSet, CompanyViewSet, DepartmentsView,
-                    DoctorsView, HealthPackagesView, HospitalDepartmentViewSet,
-                    HospitalViewSet, ItemsTarrifPriceView,
-                    LabRadiologyItemsView, SpecialisationViewSet,
+                    DoctorsView, EmergencyContactViewSet, HealthPackagesView,
+                    HospitalDepartmentViewSet, HospitalViewSet,
+                    ItemsTarrifPriceView, LabRadiologyItemsView,
+                    RequestSyncView, SpecialisationViewSet,
                     ValidateMobileOTPView, ValidateMobileView, ValidateOTPView,
                     ValidateUHIDView)
 
@@ -43,11 +44,8 @@ urlpatterns = [
     url('^validate_uhid_otp', ValidateOTPView.as_view(),
         name="validate_otp"),
 
-<<<<<<< HEAD
     url('^request_sync', RequestSyncView.as_view()),
 
-    *router.urls
-=======
     url('^generate_mobile_otp', ValidateMobileView.as_view(),
         name="validate_mobile"),
 
@@ -55,5 +53,4 @@ urlpatterns = [
         name="validate_mobile_otp"),
 
     * router.urls
->>>>>>> a746bba97ec007aae49e8f6fb4aff1b51cb85f4b
 ]
