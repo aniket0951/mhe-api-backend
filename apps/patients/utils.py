6f2b605564a90149dcb4f069c27b25654efff81c
vijay.pratap <vijay.pratap@mantralabsglobal.com>
2020-12-11 19:02:27 +0530

    added print in generate uhid

--- apps/patients/utils.py
+++ apps/patients/utils.py
@@ -15,7 +15,7 @@ def fetch_uhid_user_details(request):
     proxy_request = factory.post(
         '', {"uhid": uhid_number, "otp": otp}, format='json')
     response = ValidateOTPView().as_view()(proxy_request)
-
+    print(response.data)
     if not (response.status_code == 200 and response.data['success']):
         raise ValidationError(response.data['message'])
 
