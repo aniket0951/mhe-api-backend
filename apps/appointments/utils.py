6ed7a0e5c774b92203d504c6e3107f280915e570
Vaibhav Patel <vaibhav.p@mantralabsglobal.com>
2021-01-28 09:56:17 +0530

    Added data for feedback recipients table

--- apps/appointments/utils.py
+++ apps/appointments/utils.py
@@ -7,8 +7,12 @@ from apps.health_packages.models import HealthPackage
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from apps.patients.models import Patient
-
+from apps.master_data.models import FeedbackRecipients
 from apps.patients.exceptions import UnablToSendEmailException
+
+TO = "TO"
+CC = "CC"
+ALL = "ALL"
 
 def cancel_and_refund_parameters(param, factory=APIRequestFactory()):
     return factory.post(
@@ -60,40 +64,64 @@ def get_user_name(patient_instance):
         user_name += " "+patient_instance.last_name
     return user_name
 
-def send_feedback_received_mail(feedback_serializer):
+def get_hospital_code(patient_instance):
+    return patient_instance.favorite_hospital.code if patient_instance and patient_instance.favorite_hospital and patient_instance.favorite_hospital.code else ""
 
-    recipients = settings.FEEDBACK_NOTIFICATION_EMAIL_RECIPIENTS
-    if not recipients or not settings.FEEDBACK_NOTIFICATION_EMAIL_SUBJECT or not feedback_serializer.data.get("user_id"):
-        return
 
-    patient_instance = Patient.objects.filter(id=feedback_serializer.data.get("user_id")).first()
-    
+def get_email_body(patient_instance,feedback_serializer):
     user_name = get_user_name(patient_instance) if patient_instance else ""
     user_email = patient_instance.email if patient_instance else ""
     user_mobile = patient_instance.mobile if patient_instance else ""
+    user_uhid = patient_instance.uhid_number if patient_instance else ""
     user_favorite_hospital_name = patient_instance.favorite_hospital.description if patient_instance and patient_instance.favorite_hospital and patient_instance.favorite_hospital.description else ""
-    user_favorite_hospital_code = patient_instance.favorite_hospital.code if patient_instance and patient_instance.favorite_hospital and patient_instance.favorite_hospital.code else ""
+    user_favorite_hospital_code = get_hospital_code(patient_instance)
     user_rating = feedback_serializer.data.get("rating") or ""
     user_feedback = feedback_serializer.data.get("feedback") or ""
     user_platform = feedback_serializer.data.get("platform") or ""
     user_version = feedback_serializer.data.get("version") or ""
+    return 'Hello,\nPlease find below the feedback received from a user. \n\nUser: {}\nMobile No.: {}\nEmail: {}\nUHID: {}\nPreferred Hospital: {} ({})\nRating: {}\nFeedback: {}\nPlatform: {}\nApp Version:{}\n\nThank you!'.format(
+            user_name,
+            user_mobile,
+            user_email,
+            user_uhid,
+            user_favorite_hospital_name,
+            user_favorite_hospital_code,
+            user_rating,
+            user_feedback,
+            user_platform,
+            user_version
+        )
 
-    subject = settings.FEEDBACK_NOTIFICATION_EMAIL_SUBJECT.format(user_name)
-    
-    text_content = 'Hello,\nPlease find below the feedback received from a user. \n\nUser: {}\nMobile No.: {}\nEmail: {}\nPreferred Hospital: {} ({})\nRating: {}\nFeedback: {}\nPlatform: {}\nApp Version:{}\n\nThank you!'.format(
-        user_name,
-        user_mobile,
-        user_email,
-        user_favorite_hospital_name,
-        user_favorite_hospital_code,
-        user_rating,
-        user_feedback,
-        user_platform,
-        user_version
-    )
+def get_feedback_recipients(patient_instance):
+    hospital_code = get_hospital_code(patient_instance)
+    feedback_recipients_data = { TO:[], CC:[] }
+    feedback_recipients = FeedbackRecipients.objects.filter(hospital_code__in=[hospital_code,ALL])
+    for feedback_recipient in feedback_recipients:
+        if feedback_recipient.type and feedback_recipient.type in feedback_recipients_data:
+            feedback_recipients_data[feedback_recipient.type].append(feedback_recipient.email)
+    return feedback_recipients_data
 
-    email = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM_USER, recipients)
-    email_sent = email.send()
+def send_feedback_received_mail(feedback_serializer):
+    try:
+        recipients = settings.FEEDBACK_NOTIFICATION_EMAIL_RECIPIENTS
+        if not recipients or not settings.FEEDBACK_NOTIFICATION_EMAIL_SUBJECT or not feedback_serializer.data.get("user_id"):
+            raise UnablToSendEmailException
+        
+        patient_instance = Patient.objects.filter(id=feedback_serializer.data.get("user_id")).first()
+        if not patient_instance:
+            raise UnablToSendEmailException
+        
+        feedback_recipients_data = get_feedback_recipients(patient_instance)
+        if not feedback_recipients_data.get(TO) and not feedback_recipients_data.get(CC):
+            raise UnablToSendEmailException
+        
+        print("feedback_recipients_data",feedback_recipients_data)
+        subject = settings.FEEDBACK_NOTIFICATION_EMAIL_SUBJECT
+        text_content = get_email_body(patient_instance,feedback_serializer)
+        email = EmailMultiAlternatives(subject=subject,body=text_content,from_email=settings.EMAIL_FROM_USER,to=feedback_recipients_data.get(TO),cc=feedback_recipients_data.get(CC))
+        email_sent = email.send()
 
-    if not email_sent:
-        raise UnablToSendEmailException
+        if not email_sent:
+            raise UnablToSendEmailException
+    except Exception as e:
+        print("Exception while sending feedback email: %s"%str(e))
