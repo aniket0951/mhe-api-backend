42658d3883c99b2ab7b33c57f955594220b16ee1
Archana Bhamare <archana.b@mantralabsglobal.com>
2021-04-26 14:55:13 +0530

    Add is_corporate field in FamilyMember model

--- apps/patients/models.py
+++ apps/patients/models.py
@@ -274,6 +274,9 @@ class FamilyMember(MyBaseModel):
 
     is_visible = models.BooleanField(default=False)
 
+    is_corporate = models.BooleanField(default=False,
+                                       verbose_name='is_corporate')
+    
     raw_info_from_manipal_API = JSONField(blank=True,
                                           null=True
                                           )
