e7be6c0318d51e3ef0355c4474dfef6d99fbaeb8
PrashantSachan <prashant.s@mantralabsglobal.com>
2021-05-31 14:35:13 +0530

    Added image field in MobileNotification Table

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,8 +1,19 @@
 from django.db import models
-
+import os
+import uuid
+from utils.custom_storage import MediaStorage
+from django.core.validators import FileExtensionValidator
+from django.conf import settings
 from apps.patients.models import Patient
 from django_extensions.db.models import TimeStampedModel
+from django_clamd.validators import validate_file_infection
+from utils.validators import validate_file_authenticity, validate_file_size
 
+
+def generate_birthday_picture_path(self, filename):
+    _, obj_file_extension = os.path.splitext(filename)
+    obj_name = str(uuid.uuid4()) + str(obj_file_extension)
+    return "static/birthday/images/{0}".format(obj_name)
 
 # Create your models here.
 class MobileDevice(models.Model):
@@ -21,3 +32,16 @@ class MobileNotification(TimeStampedModel):
     title = models.CharField(max_length=512, null=True, blank=True)
     message = models.TextField()
     status = models.CharField(max_length=10, default='unread')
+    birthday_image = models.ImageField(
+                        upload_to=generate_birthday_picture_path,
+                        storage=MediaStorage(),
+                        validators=[
+                            FileExtensionValidator(settings.VALID_IMAGE_FILE_EXTENSIONS), 
+                            validate_file_size,
+                            validate_file_authenticity,
+                            validate_file_infection
+                        ],
+                        blank=True,
+                        null=True,
+                        verbose_name='Birthday Picture'
+                    )
