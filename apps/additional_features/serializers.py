a509bef8e7f3bc9389764f3eaecd7bf468d7b356
Vaibhav Patel <vaibhav.p@mantralabsglobal.com>
2021-06-16 12:59:29 +0530

    rearranged Drive inventory objects for patient user

--- apps/additional_features/serializers.py
+++ apps/additional_features/serializers.py
@@ -2,7 +2,7 @@ from apps.master_data.serializers import MedicineSerializer
 from apps.doctors.serializers import HospitalSerializer
 import logging
 
-from utils.utils import generate_pre_signed_url
+from utils.utils import generate_pre_signed_url, manipal_admin_object, patient_user_object
 from utils.serializers import DynamicFieldsModelSerializer
 from .models import Drive, DriveBilling, DriveBooking, DriveInventory, StaticInstructions
         
@@ -21,16 +21,42 @@ class DriveSerializer(DynamicFieldsModelSerializer):
             if instance.qr_code:
                 response_object['qr_code'] = generate_pre_signed_url(instance.qr_code.url)
             if instance.hospital:
-                response_object['hospital'] = HospitalSerializer(instance.hospital).data
+                    response_object['hospital'] = HospitalSerializer(instance.hospital).data
 
             drive_billing_ids = DriveBilling.objects.filter(drive_id=instance.id)
             response_object['drive_billings'] = DriveBillingSerializer(drive_billing_ids,many=True).data
 
-            drive_inventory_ids = DriveInventory.objects.filter(drive_id=instance.id)
-            response_object['drive_inventories'] = DriveInventorySerializer(drive_inventory_ids,many=True).data
+            admin_object = manipal_admin_object(self.context['request'])
+            if admin_object:
+                drive_inventory_ids = DriveInventory.objects.filter(drive_id=instance.id)
+                response_object['drive_inventories'] = DriveInventorySerializer(drive_inventory_ids,many=True).data
+
+            if patient_user_object(self.context['request']):
+                drive_inventory_ids = DriveInventory.objects.filter(drive_id=instance.id)
+                drive_inventories_list = DriveInventorySerializer(drive_inventory_ids,many=True).data
+            
+                drive_inventories_combined = {}
+                for drive_inventory in  drive_inventories_list:
+                    if drive_inventory.get("medicine").get("id") not in drive_inventories_combined:
+                        medicine_obj = drive_inventory.get("medicine")
+                        medicine_obj.pop("additional_details")
+                        drive_inventories_combined[drive_inventory.get("medicine").get("id")] = {
+                            "medicine":medicine_obj,
+                            "drive":drive_inventory.get("drive"),
+                            "inventory":[]
+                        }
+                    drive_inventories_combined[drive_inventory.get("medicine").get("id")]["inventory"].append({
+                        "id":drive_inventory.get("id"),
+                        "dose":drive_inventory.get("dose"),
+                        "mh_item_code": drive_inventory.get("mh_item_code"),
+                        "item_quantity": drive_inventory.get("item_quantity"),
+                        "price": drive_inventory.get("price")
+                    })
+
+                response_object['drive_inventories'] = [drive_inventories_combined[key] for key in drive_inventories_combined]
 
         except Exception as error:
-            logger.info("Exception in DriveSerializer: %s"%(str(error)))
+            logger.info("Exception in DriveSerializer -> to_representation: %s"%(str(error)))
             
         return response_object
 
