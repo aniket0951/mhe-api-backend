460afd8a1f1b41a14e5948796e85796595f76076
Archana Bhamare <archana.b@mantralabsglobal.com>
2021-06-25 16:28:37 +0530

    Fixed spelling

--- apps/patients/constants.py
+++ apps/patients/constants.py
@@ -3,7 +3,7 @@ class PatientsConstants:
     INVALID_OTP = "Invalid OTP is entered!"
     INVALID_REQUEST = "Invalid request!"
     REQUESTED_INFO_NOT_FOUND = "Requested information not found!"
-    OTP_EMAIL_SENT = "OTP to verify you email is sent successfully!"
+    OTP_EMAIL_SENT = "OTP to verify your email is sent successfully!"
     ENTER_VALID_UHID = "Enter valid UHID number"
     UHID_LINKED_TO_FAMILY_MEMBER = "You have an existing family member with this UHID."
     REACHED_LIMIT_FAMILY_MEMBER = "You have reached limit, you cannot add family members!"
