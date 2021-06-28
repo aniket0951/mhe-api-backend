25d3e15fad03b351a505d9bc8621216cbe43f1ce
Archana Bhamare <archana.b@mantralabsglobal.com>
2021-06-28 11:47:37 +0530

    Adding environment validation

--- apps/master_data/management/commands/generate_app_statistics.py
+++ apps/master_data/management/commands/generate_app_statistics.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+from django.conf import settings
 from django.core.management import BaseCommand, CommandError
 from rest_framework.test import APIClient
 from tablib import Dataset
@@ -19,20 +20,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            all_hospitals = Hospital.objects.all()
-            specific_date = None
-            if options and options.get('specific_date') and len(options.get('specific_date'))>0:
-                specific_date = str(options['specific_date'][0])
-            for each_hospital in all_hospitals:
-                print("Started pushing statistics for {} hospital.".format(each_hospital.code))
-                client.post('/api/master_data/patient_app_statistics',
-                                       json.dumps({
-                                            'hospital_code': each_hospital.code,
-                                            "specific_date": specific_date
-                                        }), content_type='application/json')
-                print(
-                    "Completed pushing statistics for {} hospital.\n------------".
-                    format(each_hospital.code))
+            if settings.ENVIRONMENT == "PROD":
+                all_hospitals = Hospital.objects.all()
+                specific_date = None
+                if options and options.get('specific_date') and len(options.get('specific_date'))>0:
+                    specific_date = str(options['specific_date'][0])
+                for each_hospital in all_hospitals:
+                    print("Started pushing statistics for {} hospital.".format(each_hospital.code))
+                    client.post('/api/master_data/patient_app_statistics',
+                                        json.dumps({
+                                                'hospital_code': each_hospital.code,
+                                                "specific_date": specific_date
+                                            }), content_type='application/json')
+                    print(
+                        "Completed pushing statistics for {} hospital.\n------------".
+                        format(each_hospital.code))
 
         except Exception as e:
             print(
