bcf6c91f14ffdefa8ee2239bf72c32223cfc2851
Archana Bhamare <archana.b@mantralabsglobal.com>
2021-08-16 13:52:10 +0530

    Removed print statement

--- apps/patients/views.py
+++ apps/patients/views.py
@@ -482,7 +482,6 @@ class PatientViewSet(custom_viewsets.ModelViewSet):
         random_password = get_random_string(
             length=OTP_LENGTH, allowed_chars='0123456789')
 
-        print("pasword==",random_password)
         if str(request_patient.mobile) == settings.HARDCODED_MOBILE_NO:
             random_password = settings.HARDCODED_MOBILE_OTP
 
