bcb522687280bab50b2b9baee5edbc1641cf5087
Archana Bhamare <archana.b@mantralabsglobal.com>
2021-09-21 12:35:47 +0530

    Created ReportFileViewSet API

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -13,14 +13,14 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 from utils import custom_viewsets
-from utils.custom_permissions import InternalAPICall, IsPatientUser
+from utils.custom_permissions import BlacklistDestroyMethodPermission, BlacklistUpdateMethodPermission, InternalAPICall, IsManipalAdminUser, IsPatientUser
 from utils.utils import patient_user_object
 
 from .filters import ReportFilter
-from .models import (FreeTextReportDetails, NumericReportDetails, Report,
+from .models import (ReportFile, FreeTextReportDetails, NumericReportDetails, Report,
                      ReportDocuments, StringReportDetails, TextReportDetails,
                      VisitReport)
-from .serializers import (FreeTextReportDetailsSerializer,
+from .serializers import (ReportFileSerializer, FreeTextReportDetailsSerializer,
                           NumericReportDetailsSerializer,
                           ReportDocumentsSerializer, ReportSerializer,
                           StringReportDetailsSerializer,
@@ -291,3 +291,36 @@ class ReportVisitViewSet(custom_viewsets.ModelViewSet):
             qs = qs.filter(patient_class=patient_class).distinct()
 
         return qs.filter(uhid=uhid).order_by('-created_at').distinct()
+    
+    
+class ReportFileViewSet(custom_viewsets.ModelViewSet):
+    permission_classes = [AllowAny, ]
+    model = ReportFile
+    queryset = ReportFile.objects.all().order_by('-created_at')
+    serializer_class = ReportFileSerializer
+    create_success_message = "Report file is uploaded successfully."
+    update_success_message = "Report file is updated successfully."
+    list_success_message = 'Report files returned successfully!'
+    retrieve_success_message = 'Report file information returned successfully!'
+    filter_backends = (DjangoFilterBackend,
+                       filters.SearchFilter, )
+    
+    def get_permissions(self):
+
+        if self.action in ['create','partial_update']:
+            permission_classes = [AllowAny]
+            return [permission() for permission in permission_classes]
+
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsPatientUser | IsManipalAdminUser ]
+            return [permission() for permission in permission_classes]
+        
+        if self.action == 'update':
+            permission_classes = [BlacklistUpdateMethodPermission]
+            return [permission() for permission in permission_classes]
+
+        if self.action == 'destroy':
+            permission_classes = [BlacklistDestroyMethodPermission]
+            return [permission() for permission in permission_classes]
+
+        return super().get_permissions()
