6102b2214bb54fa603259666556cf0b7a0498055
Archana Bhamare <archana.b@mantralabsglobal.com>
2021-10-14 16:25:08 +0530

    Fixed payment_details issue in AppointmentSerializer

--- apps/appointments/serializers.py
+++ apps/appointments/serializers.py
@@ -56,9 +56,10 @@ class AppointmentSerializer(DynamicFieldsModelSerializer):
             response_object["reason"] = CancellationReasonSerializer(
                 instance.reason).data
         
-        payment_instance = Payment.objects.filter(appointment=instance.id)
-        from apps.payments.serializers import PaymentSerializer
-        response_object["payment_details"] = PaymentSerializer(payment_instance, fields=(
+        payment_instance = Payment.objects.filter(appointment=instance.id).first()
+        if payment_instance:
+            from apps.payments.serializers import PaymentSerializer
+            response_object["payment_details"] = PaymentSerializer(payment_instance, fields=(
                                                 'id','razor_order_id','razor_payment_id',
                                                 'transaction_id','status',
                                                 'amount')).data
