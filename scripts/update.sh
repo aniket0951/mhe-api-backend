7b0e5e12917206d4988f4a38c8a4e129b78924a2
Debajyoti Sahani <debajyoti.s@mantralabsglobal.com>
2021-11-02 09:19:23 +0000

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/mhe-backend/mhe-api-backend
-source ../mhenv/bin/activate
-git pull
+#cd /home/ec2-user/mhe-backend/mhe-api-backend
+#source ../mhenv/bin/activate
+#git pull
 #python manage.py migrate
+echo 'this is update.sh'
