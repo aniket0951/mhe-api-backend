c457d0bd6b7998311bf0888d420dd91a5686a277
Debajyoti Sahani <debajyoti.s@mantralabsglobal.com>
2021-11-03 14:04:40 +0000

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo cp /home/ec2-user/mhe-backend/mhenv/.env /home/ec2-user/mhe-backend/mhe-api-backend/
-sudo cp -r /home/ec2-user/mhe-backend/mhenv/logs /home/ec2-user/mhe-backend/mhe-api-backend/logs
+#sudo cp -r /home/ec2-user/mhe-backend/mhenv/logs /home/ec2-user/mhe-backend/mhe-api-backend/logs
 cd /home/ec2-user/mhe-backend/mhe-api-backend/
-sudo chomd 777 -R .
+sudo chmod 777 -R .
 source ../mhenv/bin/activate
 python manage.py migrate
 #python manage.py loaddata apps/master_data/fixtures/component_fixture.json
