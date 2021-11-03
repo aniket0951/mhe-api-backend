32f575b9995443902f79bdf1f5e9a9b6ff66c4b3
Debajyoti Sahani <debajyoti.s@mantralabsglobal.com>
2021-11-03 12:54:28 +0000

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/mhe-backend/mhe-api-backend
-source ../mhenv/bin/activate
-#git pull
-python manage.py migrate
-#python manage.py loaddata apps/master_data/fixtures/component_fixture.json
+cd /home/ec2-user/mhe-backend/
+sudo rm -rf mhe-api-backend
+
