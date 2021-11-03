07c5eb087c3899259905ac3dcec029de009df93c
Archana Bhamare <archana.b@mantralabsglobal.com>
2021-11-03 14:31:51 +0530

    Added type in components fixture

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,4 +3,5 @@ cd /home/ec2-user/mhe-backend/mhe-api-backend/
 source ../mhenv/bin/activate
 git pull
 python manage.py migrate
+python manage.py loaddata apps/master_data/fixtures/component_fixture.json
 
