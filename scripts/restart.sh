9c46d69960fa35b6554fccece51f80f54dad9dbc
Debajyoti Sahani <debajyoti.s@mantralabsglobal.com>
2021-11-03 12:55:50 +0000

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+cd /home/ec2-user/mhe-backend/mhe-api-backend
+source ../mhenv/bin/activate
+python manage.py migrate
+#python manage.py loaddata apps/master_data/fixtures/component_fixture.json
 sudo service gunicorn restart
 sudo service celery restart 
