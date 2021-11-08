5708ea80450a6ddfa11bd7e227bc3cc11a0922b6
Debajyoti Sahani <debajyoti.s@mantralabsglobal.com>
2021-11-08 08:00:03 +0000

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo cp -r /home/ec2-user/mhe-backend/mhe-api-backend/logs /home/ec2-user/mhe-backend/mhenv/logs
 cd /home/ec2-user/mhe-backend/
+sudo mkdir deba
 sudo rm -rf mhe-api-backend
 
