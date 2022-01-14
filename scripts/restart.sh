f3b94140557fffb07d73ec1111b6652c9b430d56
Archana Bhamare <archana.b@mantralabsglobal.com>
2022-01-14 11:04:06 +0530

    Added

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -5,7 +5,6 @@ cd /home/ec2-user/mhe-backend/mhe-api-backend/
 sudo chmod 777 -R .
 source ../mhenv/bin/activate
 python manage.py migrate
-python manage.py loaddata apps/master_data/fixtures/ambulance_contact.json
-python manage.py loaddata apps/master_data/fixtures/helpline_numbers_fixture_test.json
+python manage.py loaddata apps/master_data/fixtures/hospitals.json
 sudo service gunicorn restart
 sudo service celery restart
