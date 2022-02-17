6c12769f8fc4dc7ad90d0be79c9dff4f8a118850
Archana Bharat  Bhamare <archana.b@mantralabs.tech>
2022-02-17 15:29:55 +0530

    Modified

--- apps/master_data/management/commands/create_or_update_health_packages.py
+++ apps/master_data/management/commands/create_or_update_health_packages.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from django.core.management import BaseCommand, CommandError
+from apps.health_tests.models import HealthTest
 from rest_framework.test import APIClient
 from tablib import Dataset
 
@@ -20,6 +21,9 @@ class Command(BaseCommand):
         try:
             all_hospitals = Hospital.objects.all()
             logger.info("all_hospitals -- %s"%(str(all_hospitals)))
+            all_health_test = HealthTest.objects.all()
+            all_health_test.delete()
+          #  all_health_test.save()
             for each_hospital in all_hospitals:
                 logger.info("each_hospital -- %s"%(str(each_hospital)))
                 print("Started loading Health Packages of {} hospital.".format(
